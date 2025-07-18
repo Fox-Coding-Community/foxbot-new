5c739a4570081320c8f1da8eda7f769edcb7883c
Morgan <75649447+iMorganSo@users.noreply.github.com>
2025-07-19 00:31:30 +0300

    Update config.js
    removed DB section and moved it into DOTENV(.env)

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports ={
     "GuildID": "1277221841491984416",
     "ClientID": "1276940389537812555",
-    "db": "mongodb+srv://morjanmj11:YWFObKxoMmD4pXtJ@cluster.bru94.mongodb.net/",
     "developers": ["1105670596525834250", "743809739703451749"]
 };
 
